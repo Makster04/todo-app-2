af70f3634ff7e945420d97dd5c949fee0bb4bc2f
Mak Trnka <maktrnka@comcast.net>
2024-05-16 09:08:27 -0700

    Update form.tsx

--- src/hooks/form.tsx
+++ src/hooks/form.tsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 
 const useForm = (callback: (values: any) => void, defaultValues: any = {}) => {
+
   const [values, setValues] = useState({});
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    callback({ ...values });
+    callback({...values});
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement> | number) => {
-    // Implementation of handleChange function
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setValues(prevValues => ({ ...prevValues, [name]: value }));
   };
 
   useEffect(() => {
